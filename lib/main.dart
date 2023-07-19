58a99487a6168878a246053821458496a9bfe7ad
WinnMBG <winn77.m@hotmail.com>
2023-07-19 23:40:58 +0200

    create router

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_cine/movieslist.dart';
-import 'package:flutter_application_cine/notifiers/searchmovies.dart';
-import 'package:flutter_application_cine/searchbar.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_application_cine/pages/homescreen.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+        path: '/favorites',
+        builder: (context, state) => Column(children: [
+              Text('je suis la page des favoris'),
+              TextButton(
+                onPressed: () => context.go('/'),
+                child: Text('Go back home'),
+              ),
+              TextButton(
+                onPressed: () => context.go('/boubou'),
+                child: Text('Boubou?'),
+              )
+            ])),
+    GoRoute(
+        path: '/boubou',
+        builder: (context, state) => Column(children: [
+              Text('je suis Boubou'),
+              TextButton(
+                onPressed: () => context.go('/'),
+                child: Text('Go back home'),
+              ),
+              TextButton(
+                onPressed: () => context.go('/favorites'),
+                child: Text('Go to favorites ?'),
+              )
+            ])),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'CineDuRat',
-        debugShowCheckedModeBanner: false,
-        // theme: ThemeData(scaffoldBackgroundColor: Colors.deepPurple),
-        home: Stack(children: <Widget>[
-          CarouselSlider(
-            options: CarouselOptions(
-              height: MediaQuery.of(context).size.height * 1,
-              autoPlay: true,
-              viewportFraction: 1.0,
-              enlargeCenterPage: false,
-              // autoPlay: false,
-            ),
-            items: [
-              "images/cine1.jpg",
-              "images/cine2.jpg",
-              "images/cine3.jpg",
-              // "https://newdawnfilm.com/wp-content/uploads/2020/06/shutterstock_169841813-min.png",
-              // "https://www.cia-france.fr/media/1492/les-films-incontournables-du-cinema-w_2524x884.jpg",
-            ]
-                .map((item) => Container(
-                      child: Center(
-                        child: Image.asset(
-                          item,
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ))
-                .toList(),
-          ),
-          Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                backgroundColor: Colors.black54,
-                title: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 6),
-                      child: Image.network(
-                        "https://th.bing.com/th/id/OIP.19eOcs_L9HWel9NYlfZZrwHaHa?w=195&h=195&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-                        width: 20,
-                        height: 20,
-                      ),
-                    ),
-                    Text('CinéRat')
-                  ],
-                ),
-                actions: <Widget>[
-                  IconButton(
-                    icon: const Icon(
-                      Icons.favorite,
-                      color: Colors.redAccent,
-                    ),
-                    onPressed: () {
-                      // do something
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.home_filled,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      // do something
-                    },
-                  )
-                ],
-              ),
-              body: ChangeNotifierProvider(
-                  create: (BuildContext context) {
-                    return MoviesModel();
-                  },
-                  child: ListView(children: [SearchBarClass(), MoviesList()])))
-        ]));
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'CineDuRat',
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(scaffoldBackgroundColor: Colors.deepPurple),
+    );
   }
 }
---
+++ lib/pages/homescreen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_cine/movieslist.dart';
+import 'package:flutter_application_cine/notifiers/searchmovies.dart';
+import 'package:flutter_application_cine/searchbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      CarouselSlider(
+        options: CarouselOptions(
+          height: MediaQuery.of(context).size.height * 1,
+          autoPlay: true,
+          viewportFraction: 1.0,
+          enlargeCenterPage: false,
+          // autoPlay: false,
+        ),
+        items: [
+          "images/cine1.jpg",
+          "images/cine2.jpg",
+          "images/cine3.jpg",
+          // "https://newdawnfilm.com/wp-content/uploads/2020/06/shutterstock_169841813-min.png",
+          // "https://www.cia-france.fr/media/1492/les-films-incontournables-du-cinema-w_2524x884.jpg",
+        ]
+            .map((item) => Container(
+                  child: Center(
+                    child: Image.asset(
+                      item,
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ))
+            .toList(),
+      ),
+      Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.black54,
+            title: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 6),
+                  child: Image.network(
+                    "https://th.bing.com/th/id/OIP.19eOcs_L9HWel9NYlfZZrwHaHa?w=195&h=195&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+                    width: 20,
+                    height: 20,
+                  ),
+                ),
+                Text('CinéRat')
+              ],
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.redAccent,
+                ),
+                onPressed: () => context.go('/favorites'),
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.home_filled,
+                  color: Colors.white,
+                ),
+                onPressed: () => context.go('/'),
+              )
+            ],
+          ),
+          body: ChangeNotifierProvider(
+              create: (BuildContext context) {
+                return MoviesModel();
+              },
+              child: ListView(children: [SearchBarClass(), MoviesList()])))
+    ]);
+  }
+}
