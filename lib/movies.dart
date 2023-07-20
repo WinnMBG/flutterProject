c65111660699a30285a73f9d03ae957836712fff
WinnMBG <winn77.m@hotmail.com>
2023-07-20 10:19:35 +0200

    add image button

--- lib/movies.dart
+++ lib/movies.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Movie extends StatelessWidget {
   dynamic data;
 
   Movie(this.data, {super.key});
 
-  Widget createMovieCard(dynamic data) {
+  Widget createMovieCard(dynamic data, BuildContext context) {
     String imgUrl = data['poster_path'] != null
         ? "https://image.tmdb.org/t/p/w500${data['poster_path']}"
         : "";
@@ -18,11 +19,14 @@ class Movie extends StatelessWidget {
         : Image.asset("images/poster.jpg", height: 350, width: 350);
     return SizedBox(
       width: 350,
-      height: 400,
+      height: 450,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          img,
+          InkWell(
+            onTap: () => context.go('/favorites'),
+            child: img,
+          ),
           Padding(
               padding: EdgeInsets.only(top: 10),
               child: Text(data['original_title'],
@@ -34,6 +38,6 @@ class Movie extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return createMovieCard(data);
+    return createMovieCard(data, context);
   }
 }
